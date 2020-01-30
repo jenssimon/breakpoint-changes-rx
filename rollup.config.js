ca46546ba99c589e9a3429c754afe2845671cd49
Jens Simon <jens.simon@deptagency.com>
2020-01-31 00:54:00 +0100

    Fix build errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from '@rollup/plugin-commonjs';
 import Typescript from 'typescript';
 import pkg from './package.json';
 
 export default [
-  // browser-friendly UMD build
-  /* {
-    input: 'src/index.ts',
-    output: {
-      name: 'BreakpointChangesRX',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [
-      typescript({
-        typescript: Typescript,
-      }),
-      commonjs(),
-    ],
-  }, */
-
   {
     input: 'src/index.ts',
     external: [],
